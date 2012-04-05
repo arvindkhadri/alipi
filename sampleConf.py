ab576856a54b0e364536572af0545443442664f9
Arvind <arvind@servelots.com>
2012-04-05 15:07:46 +0530

    Changed the path of sample config file

--- sampleConf.py
+++
@@ -1,3 +0,0 @@
-#This is a samply config file.  Create a file named conf.py,in this directory, and decalre the following variables there.
-APPURL = ('http://localhost/',) # The path which points to "alipi" directory. 
-JQUERYURL = ('http://localhost/server/apis/',) #URL for jquery.
