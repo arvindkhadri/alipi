4dde4134e0bbbed8047373421b9de5b6a5a0980f
Arvind <arvindkhadri@gmail.com>
2013-07-08 19:57:05 +0530

    Adding README and requirements.txt.

--- alipi/sampleConfig.js
+++ alipi/sampleConfig.js
@@ -1,6 +1,13 @@
 //A sample configuration file for JS variables.  Copy this file to "config.js"
+/*config['hostname'] = "hostname of the domain from the application is served"
+ * config['deploy'] = "URL of where the application is deployed, same as DEPLOYURL in conf.py"
+ * config['root'] = "Same as APPURL from conf.py"
+ * config['sweet'] = "URL for the sweet store, same as SWEETURL from conf.py"
+ */
 var config = {
-    'hostname': "127.0.0.1",
-    'deploy': "http://127.0.0.1:5000",
-    'root': "http://localhost"
+    'hostname': "y.a11y.in",
+    'deploy': "http://y.a11y.in/web",
+		'root': "http://y.a11y.in",
+		'sweet': "http://demo.swtr.us"
 }
+
